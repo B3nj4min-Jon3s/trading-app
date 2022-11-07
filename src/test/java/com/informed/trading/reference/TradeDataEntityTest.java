b07f9200a283899ae0fab87829f4e2ab8bbf9c20
Ben Jones <ben.jones@informed.com>
2022-11-07 10:39:16 +0000

    Added tests for Trade Data Entity

--- src/test/java/com/informed/trading/reference/TradeDataEntityTest.java
+++ src/test/java/com/informed/trading/reference/TradeDataEntityTest.java
@@ -1,6 +1,9 @@
 package com.informed.trading.reference;
 
+import com.informed.trading.exception.EmptyArgumentException;
+import com.informed.trading.exception.InvalidArgumentException;
 import com.informed.trading.exception.InvalidSymbolArgumentException;
+import com.informed.trading.reference.tradedata.TradeDataEntity;
 import com.informed.trading.samples.SampleTradeDataEntity;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,22 +27,69 @@ class TradeDataEntityTest {
     }
 
     @Nested
-    public class constructorTests {
+    public class Constructor {
 
-        @Test
-        void shouldGenerateRandomIdWhenNotSupplied() {
-            sampleTadeEntity = new SampleTradeDataEntity("Manchester", "MAN");
-            assertNotEquals(0, sampleTadeEntity.getId());
+        @Nested
+        public class SymbolTests {
+            @Test
+            void shouldThrowSymbolArgumentExceptionWhenSymbolEmptyString() {
+                assertThrows(EmptyArgumentException.class, () -> new SampleTradeDataEntity("Manchester", ""));
+            }
+
+            @Test
+            void shouldThrowNullPointerExceptionWhenSymbolNull() {
+                assertThrows(NullPointerException.class, () -> new SampleTradeDataEntity("Manchester", null));
+            }
+
+            @Test
+            void shouldThrowInvalidArgumentExceptionWhenLongerThanThreeChars() {
+                assertThrows(InvalidSymbolArgumentException.class, () -> new SampleTradeDataEntity("Manchester", "HHHH"));
+            }
+
+            @Test
+            void shouldThrowInvalidArgumentExceptionWithIllegalChars() {
+                assertThrows(InvalidSymbolArgumentException.class, () -> new SampleTradeDataEntity("Manchester", "H%H"));
+            }
+
+            @Test
+            void shouldThrowInvalidArgumentExceptionWithNumbers() {
+                assertThrows(InvalidSymbolArgumentException.class, () -> new SampleTradeDataEntity("Manchester", "2GH"));
+            }
+
+            @Test
+            void shouldSetSymbolToUpperCase() {
+                TradeDataEntity testEntity = new SampleTradeDataEntity("Manchester", "man");
+                assertEquals("MAN", testEntity.getSymbol());
+            }
         }
 
-        @Test
-        void shouldThrowSymbolArgumentExceptionWhenSymbolEmptyString() {
-            assertThrows(InvalidSymbolArgumentException.class, () -> new SampleTradeDataEntity("Manchester", ""));
-        }
+        @Nested
+        public class NameTests {
+            @Test
+            void shouldThrowNullPointerExceptionWhenNameNull() {
+                assertThrows(NullPointerException.class, () -> new SampleTradeDataEntity(null, "MAN"));
+            }
 
-        @Test
-        void shouldThrowNullPointerExceptionWhenSymbolNull() {
-            assertThrows(NullPointerException.class, () -> new SampleTradeDataEntity("Manchester", null));
+            @Test
+            void shouldThrowEmptyArgumentExceptionWhenNameEmpty() {
+                assertThrows(EmptyArgumentException.class, () -> new SampleTradeDataEntity("", "MAN"));
+            }
+
+            @Test
+            void shouldThrowInvalidArgumentExceptionWhenNumbers() {
+                assertThrows(InvalidArgumentException.class, () -> new SampleTradeDataEntity("M4nchester", "MAN"));
+            }
+
+            @Test
+            void shouldThrowInvalidArgumentExceptionWhenIllegalChars() {
+                assertThrows(InvalidArgumentException.class, () -> new SampleTradeDataEntity("Manchest%er", "MAN"));
+            }
+
+            @Test
+            void shouldThrowInvalidArgumentExceptionWhenLongerThan50() {
+                assertThrows(InvalidArgumentException.class, () -> new SampleTradeDataEntity("Manchesterrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr", "MAN"));
+            }
+
         }
     }
 }

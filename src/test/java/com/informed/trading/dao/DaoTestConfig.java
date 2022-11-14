e8e70075b29cc178b0efd54e7126586de37d03b0
Batherag <agatha.bather@informed.com>
2022-11-14 16:28:43 +0000

    created test for the address dao

--- src/test/java/com/informed/trading/dao/DaoTestConfig.java
+++ src/test/java/com/informed/trading/dao/DaoTestConfig.java
@@ -1,5 +1,4 @@
-package com.informed.trading.service;
-
+package com.informed.trading.dao;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,8 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @DataJpaTest
 @PropertySource("classpath:TestConfig.properties") //classpath:jdbc.TestConfig.properties.properties
-@ComponentScan(basePackages = {"com.informed.trading.dao", "com.informed.trading.config"})
+@ComponentScan(basePackages = {"com.informed.trading.dao", "com.informed.trading.service"})
 @EntityScan(basePackages = {"com.informed.trading.reference"})
 @EnableJpaRepositories(basePackages = {"com.informed.trading.repo"})
-public class ServiceTestConfig {
+public class DaoTestConfig {
 }

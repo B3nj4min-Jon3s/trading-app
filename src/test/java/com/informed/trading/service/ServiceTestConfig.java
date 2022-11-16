1774b168f2b49ef1baf8ecb1f09d35a7b4e33aa8
Batherag <agatha.bather@informed.com>
2022-11-16 10:32:46 +0000

    controller test fix

--- src/test/java/com/informed/trading/service/ServiceTestConfig.java
+++ src/test/java/com/informed/trading/service/ServiceTestConfig.java
@@ -9,7 +9,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @DataJpaTest
 @PropertySource("classpath:TestConfig.properties") //classpath:jdbc.TestConfig.properties.properties
+<<<<<<< Updated upstream
 @ComponentScan(basePackages = {"com.informed.trading.dao", "com.informed.trading.config"})
+=======
+@ComponentScan(basePackages = {
+        "com.informed.trading.dao",
+        "com.informed.trading.service",
+        "com.informed.trading.controller",
+        "com.informed.trading.config.exchange"})
+>>>>>>> Stashed changes
 @EntityScan(basePackages = {"com.informed.trading.reference"})
 @EnableJpaRepositories(basePackages = {"com.informed.trading.repo"})
 public class ServiceTestConfig {
